0e2bea45402ea2288516826b601f732b820b1593
Mike McAllister <mike@logikbox.com>
2018-06-16 14:07:27 -0400

    all the other files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet" /> 
+
+</head>
+<body>
+
+<h1>Hello world</h1>
+
+<p>My first paragraph.</p>
+
+</body>
+    <script src="./js/bundle.js" type="text/javascript"></script>
+    <script>
+    $( document ).ready(function() {
+        console.log( "ready!" );
+    });
+    </script>
+</html>
