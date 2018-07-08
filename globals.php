92d4cad8bfbb248b6d3e7f82a546f16f5b22ca4d
Mike McAllister <mike@logikbox.com>
2018-07-08 12:26:03 -0400

    better logikbox/local integration

--- globals.php
+++ globals.php
@@ -3,6 +3,13 @@
 session_start();
 
 $sitename = "C&Oslash;&Oslash;KB&Oslash;&Oslash;K";
-$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/ckbk/";
+//$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]/ckbk/";
+
+//Local vs. Logikbox
+if(strpos($thisServer, 'logikbox') !== false){
+    $basehref = "http://wwww.logikbox.com/ckbk/";    
+}else{
+    $basehref = "http://localhost:8888/";  
+}
 
 ?>
