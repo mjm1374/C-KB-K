4607e76f7990cd6a177672a00c45f51a80311ccf
Mike McAllister <mike@logikbox.com>
2018-07-15 11:32:53 -0400

    nav fixed and header squared up

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <svg id="logo" height="70" width="70"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >
     <image x="0" y="0" height="70" width="70"  xlink:href="img/tomato.svg" />
 </svg>
+<div class="cbk-title-holder">
+    <h1><?php echo $sitename ?></h1>
+</div>
 
-<h1><?php echo $sitename ?></h1>
 
